6eac992ac7037c84dae32aaabf598730d8a52e50
cgarg <charu_garg@intuit.com>
2017-10-24 15:43:52 +0530

    Created first MicroService

--- src/main/java/com/demo/springboot/demo/Hello.java
+++ src/main/java/com/demo/springboot/demo/Hello.java
@@ -1,20 +1,29 @@
 package com.demo.springboot.demo;
 
 import com.sun.xml.internal.ws.api.ha.StickyFeature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class Hello {
 
-    @RequestMapping(value = "/hello")
-    public String sayHello(){
-        return "Hello Spring Boot";
-    }
+    @Value("${service1.host}")
+    private String service1Host;
 
-    @RequestMapping(value = "/bye/{name}")
-    public String sayBye(@PathVariable(name="name") String name){
-        return "Bye" + name;
+    @Value("${service1.port}")
+    private String service1Port;
+
+    private RestTemplate template = new RestTemplate();
+
+    @RequestMapping("/{name}")
+    public String getNameOutput(@PathVariable(name="name") String name){
+        String output=null;
+        output= template.getForObject("http://"+ service1Host +":"+service1Port +"/hello?name="+name,String.class);
+
+        return output;
     }
 }
